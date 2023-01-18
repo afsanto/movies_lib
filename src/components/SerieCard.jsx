bbafeb4ee10c270398b040dded51d541130da031
Adilson Santos <105400615+afsanto@users.noreply.github.com>
2023-01-18 18:53:55 -0300

    Update SerieCard.jsx

--- src/components/SerieCard.jsx
+++ src/components/SerieCard.jsx
@@ -2,17 +2,19 @@ import { Link } from "react-router-dom";
 
 import { FaStar } from "react-icons/fa";
 
+import "../pages/Movie.css";
+
 const imagesURL = import.meta.env.VITE_IMG;
 
 const SerieCard = ({ serie, showLink = true }) => {
   return (
     <div className="movie-card">
-      <img src={imagesURL + serie.poster_path} alt={serie.name} />
-      <h2>{serie.name}</h2>
+      <img   className="imgcard" src={imagesURL + serie.poster_path} alt={serie.name} />
+      <h2 className="cardtitle" >{serie.name}</h2>
       <p>
         <FaStar /> {serie.vote_average}
       </p>
-      {showLink && <Link to={`/serie/${serie.id}`}>Detalhes</Link>}
+      {showLink && <Link  className="details" to={`/serie/${serie.id}`}>Detalhes</Link>}
     </div>
   );
 };
