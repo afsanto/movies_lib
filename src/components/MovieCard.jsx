9044af6a125ac980763f4e8b3b75a237180a3f67
Adilson Santos <105400615+afsanto@users.noreply.github.com>
2023-01-19 15:18:24 -0300

    Update MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,7 +12,7 @@ const MovieCard = ({ movie, showLink = true }) => {
       <img className="imgcard"  src={imagesURL + movie.poster_path} alt={movie.title} />
       <h2 className="cardtitle"  >{movie.title}</h2>
       <p>
-        <FaStar style={{ display:"block", textAlign:"center" }} /> {movie.vote_average}
+        <FaStar style={{ display:"flex", justifyContent:"center" }} /> {movie.vote_average}
       </p>
       {showLink && <Link className="details" to={`/movie/${movie.id}`}>Detalhes</Link>}
     </div>
